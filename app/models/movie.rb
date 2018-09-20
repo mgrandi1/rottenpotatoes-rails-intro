a957419ef653c49fcfeed88f36bd7e887e250396
mgrandi1 <mgrandi@citadel.edu>
2018-09-20 20:16:48 +0000

    yeah

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,5 @@
 class Movie < ActiveRecord::Base
-    attr_accessor :title, :rating, :description, :release_date
-    
-    def self.ratingz
-        return ["G","PG","PG-13","R"]
+    def self.all_ratings
+      ["G", "PG", "PG-13", "R"]
     end
-    
 end
