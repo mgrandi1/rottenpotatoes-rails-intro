5f86ca224fa7c7c848b57cec7cbadc8de6667f60
mgrandi1 <mgrandi@citadel.edu>
2018-10-01 01:00:48 +0000

    yeah

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,24 +11,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.ratings
-    session[:ratings] = params[:ratings] unless params[:ratings].nil?
-    session[:sort] = params[:sort] unless params[:sort].nil?
+    
+    @all_ratings = Movie.all_ratings
+    
 
-    if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:sort].nil? && !session[:sort].nil?)
-      redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
-    elsif !params[:ratings].nil? || !params[:sort].nil?
-      if !params[:ratings].nil?
-        array_ratings = params[:ratings].keys
-        return @movies = Movie.where(rating: array_ratings).order(session[:sort])
-      else
-        return @movies = Movie.all.order(session[:sort])
-      end
-    elsif !session[:ratings].nil? || !session[:sort].nil?
-      redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
+    if (params[:sort])
+      @sortby = params[:sort].to_s
+      @movies = Movie.order(@sortby)
+      @sort_column = params[:sort].to_s
+    elsif (params[:ratings]) 
+      @selected = params[:ratings].keys
+      @movies = Movie.where(rating: @selected)
     else
-      return @movies = Movie.all
+      @movies = Movie.all
     end
+  end
 
   def new
     # default: render 'new' template
