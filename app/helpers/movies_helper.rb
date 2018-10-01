a0f23a88e02c9128d340bcd280ac1c0c813f24f8
mgrandi1 <mgrandi@citadel.edu>
2018-10-01 01:19:16 +0000

    yeah

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,16 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def chosen_rating?(rating)
+    chosen_ratings = session[:ratings]
+    return true if chosen_ratings.nil?
+    chosen_ratings.include? rating
+  end
+  def hilite(column)
+    if(session[:sort].to_s == column)
+      return 'hilite'
+    else
+      return nil
+    end
+  end
 end
